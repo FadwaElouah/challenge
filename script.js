ae8592e2fd31cbdc639148b3c97a9d5f845b5e93
FadwaElouah <fadwaelouah@gmail.com>
2024-10-12 20:05:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+//    exercice 1 
+//  var prenom = "Fadwa";
+//  console.log(prenom)
+
+
+
+// exercice 2
+// var a = 8;
+// var b = 4;
+// var somme = a + b ;
+// console.log(somme)
+
+// var difference = a - b;
+// console.log(difference)
+
+// var produit = a * b ;
+// console.log(produit)
+
+// var  quotient = a / b ;
+// console.log(quotient)
+
+
+
+
+// exercice 3:
+// var nom = "ellouah"
+// var prenom = "fadwa";
+// var nomComplet = nom + " " + prenom
+// console.log(nomComplet)
+
+
+
+
+// exercice 4:
+// var age = 24;
+// var majeur = age >= 18;
+// console.log(majeur)
+
+
+
+
+// exercice 5:
+// var x = 10 ;
+// var y = 2;
+//  x = x + y ;
+//  y = x - y ;
+//  x = x - y ;
+//  console.log("la nouvelle valeur de x est  = "+ x)
+//  console.log("la nouvelle valeur de y est = "+ y)
+
+
+
+
+
+// exercice 6;
+// var rayon = 6;
+// var circonference = (2 * Math.PI * rayon)
+// console.log( "circonférence d'un circle est " + circonference)
+
+
+
+// exercice 7:
+// var celsius = 4;
+// var F = ( celsius * (9/5) + 32)
+// console.log(F)
+
+
+
+
+// exercice 8;
+// var anneeNaissance = 2000;
+// var anneeActuelle = 2024;
+// var age = anneeActuelle - anneeNaissance;
+// console.log(age)
+
+
+
+
+
+// exercice 9 :
+//  var longueur = 6;
+//  var largeur = 3 ;
+//  var P = 2 * (longueur + largeur);
+//  console.log(" le périmètre du rectangle est " + P)
+
+
+
+// exercice 10:
+// var note1 = 16;
+// var note2 = 17;
+// var note3 = 18;
+// var somme = note1 + note2 + note3;
+// var Moyenne = somme/3;
+// console.log("la moyenne est "+ Moyenne)
